8c17065f60df828d6ea6bd0d1019d7040bcc81b4
Hugo A Guerra <guerrahugoa@gmail.com>
2016-01-18 15:40:09 -0800

    added if/else logic to randomizer in order to avoid duplicate images

--- js/app.js
+++ js/app.js
@@ -11,18 +11,18 @@ var globalClicksTracker = 0; //tracks number of products clicked, regardless of 
 
 //all Product Objects are created below and pushed to prodArray
 prodArray[0] = new Product ('R2D2 Bag', 'img/prod1.jpg');
-prodArray[1] = new Product ('Banana Slicer', 'img/prod1.jpg');
-prodArray[2] = new Product ('Open Toed Boots', 'img/prod2.jpg');
-prodArray[3] = new Product ('Convex Chair', 'img/prod3.jpg');
-prodArray[4] = new Product ('Chtulu Doll', 'img/prod4.jpg');
-prodArray[5] = new Product ('Dragon Meat Can', 'img/prod5.jpg');
-prodArray[6] = new Product ('Pen Cap Utensils', 'img/prod6.jpg');
-prodArray[7] = new Product ('Pizza Cutter', 'img/prod7.jpg');
-prodArray[8] = new Product ('Shark Sleeping Bag', 'img/prod8.jpg');
-prodArray[9] = new Product ('Baby Sweeper Onesie', 'img/prod9.jpg');
-prodArray[10] = new Product ('Unicorn Meat Can', 'img/prod10.png');
-prodArray[11] = new Product ('USB Tentacle Flash Drive', 'img/prod11.jpg');
-prodArray[12] = new Product ('Self Watering Can', 'img/prod13.gif');
+prodArray[1] = new Product ('Banana Slicer', 'img/prod2.jpg');
+prodArray[2] = new Product ('Open Toed Boots', 'img/prod3.jpg');
+prodArray[3] = new Product ('Convex Chair', 'img/prod4.jpg');
+prodArray[4] = new Product ('Chtulu Doll', 'img/prod5.jpg');
+prodArray[5] = new Product ('Dragon Meat Can', 'img/prod6.jpg');
+prodArray[6] = new Product ('Pen Cap Utensils', 'img/prod7.jpg');
+prodArray[7] = new Product ('Pizza Cutter', 'img/prod8.jpg');
+prodArray[8] = new Product ('Shark Sleeping Bag', 'img/prod9.jpg');
+prodArray[9] = new Product ('Baby Sweeper Onesie', 'img/prod10.png');
+prodArray[10] = new Product ('Unicorn Meat Can', 'img/prod11.jpg');
+prodArray[11] = new Product ('USB Tentacle Flash Drive', 'img/prod12.gif');
+prodArray[12] = new Product ('Self Watering Can', 'img/prod13.jpg');
 prodArray[13] = new Product ('Death Star Wine Glass', 'img/prod14.jpg');
 
 var displayWindowOne = document.getElementById('productOne');
@@ -32,9 +32,23 @@ var displayWindowThree = document.getElementById('productThree');
 
 function randomProductsDisplay () {
 
- displayWindowOne.innerHTML = '<img src="' + prodArray[Math.floor(Math.random() * 14)].filePath +'" alt = "">'
- displayWindowTwo.innerHTML = '<img src="' + prodArray[Math.floor(Math.random() * 14)].filePath +'" alt = "">'
- displayWindowThree.innerHTML = '<img src="' + prodArray[Math.floor(Math.random() * 14)].filePath +'" alt = "">'
+ var randNumOne = Math.floor(Math.random() * 14);
+ displayWindowOne.innerHTML = '<img src="' + prodArray[randNumOne].filePath +'" alt = "">';
+ var randNumTwo = Math.floor(Math.random() * 14);
+ if (randNumTwo === randNumOne) {
+   var randNumTwo = Math.floor(Math.random() * 14);
+   displayWindowTwo.innerHTML = '<img src="' + prodArray[randNumTwo].filePath +'" alt = "">';
+ } else {
+   displayWindowTwo.innerHTML = '<img src="' + prodArray[randNumTwo].filePath +'" alt = "">';
+ };
+ var randNumThree = Math.floor(Math.random() * 14);
+ if (randNumThree === randNumTwo || randNumThree === randNumOne) {
+   var randNumThree = Math.floor(Math.random() * 14);
+   displayWindowThree.innerHTML = '<img src="' + prodArray[randNumThree].filePath +'" alt = "">';
+ } else {
+   displayWindowThree.innerHTML = '<img src="' + prodArray[randNumThree].filePath +'" alt = "">';
+ };
+ // displayWindowThree.innerHTML = '<img src="' + prodArray[Math.floor(Math.random() * 14)].filePath +'" alt = "">'
 }
 
 randomProductsDisplay();
