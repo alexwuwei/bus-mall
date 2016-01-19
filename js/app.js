3e2b810a8b684659a31ee8dbc41b014e66e4f97d
Hugo A Guerra <guerrahugoa@gmail.com>
2016-01-19 12:25:46 -0800

    added additional variables to track. Also added media query for size adjustment.

--- js/app.js
+++ js/app.js
@@ -2,12 +2,20 @@ var prodArray = []; //stores Product Objects
 var globalRandNumOne = 0;
 var globalRandNumTwo = 0;
 var globalRandNumThree = 0;
+var globalLeftClickTracker = 0; //global tracker for number of container clicks
+var globalCenterClickTracker = 0; //global tracker for number of container clicks
+var globalRightClickTracker = 0; //global tracker for number of container clicks
 
 //Product Constructor defined below
 function Product (productName, filePath) {
   this.productName = productName;
   this.filePath = filePath;
   this.productClicksTracker = 0;
+  this.productImpressionsTracker = 0;
+  this.rightClickTracker = 0;
+  this.centerClickTracker = 0;
+  this.leftClickTracker = 0;
+  // this.conversionRatio = parseFloat(this.productImpressionsTracker) / parseFloat(this.productClicksTracker);
 }
 
 var globalClicksTracker = 0; //tracks number of products clicked, regardless of selection
@@ -38,24 +46,28 @@ function randomProductsDisplay () {
  var randNumOne = Math.floor(Math.random() * 14);
  globalRandNumOne = randNumOne; //saves random number for later use
  displayWindowOne.innerHTML = '<img src="' + prodArray[randNumOne].filePath +'" alt = "">';
- var randNumTwo = Math.floor(Math.random() * 14);
+ var randNumTwo = Math.floor(Math.random() * 14); //saves random number for later use
  if (randNumTwo === randNumOne) {
    var randNumTwo = Math.floor(Math.random() * 14);
-   globalRandNumTwo = randNumTwo;
+   globalRandNumTwo = randNumTwo; //saves random number for later use
    displayWindowTwo.innerHTML = '<img src="' + prodArray[randNumTwo].filePath +'" alt = "">';
  } else {
-   globalRandNumTwo = randNumTwo;
+   globalRandNumTwo = randNumTwo; //saves random number for later use
    displayWindowTwo.innerHTML = '<img src="' + prodArray[randNumTwo].filePath +'" alt = "">';
  };
  var randNumThree = Math.floor(Math.random() * 14);
  if (randNumThree === randNumTwo || randNumThree === randNumOne) {
    var randNumThree = Math.floor(Math.random() * 14);
-   globalRandNumThree = randNumThree;
+   globalRandNumThree = randNumThree; //saves random number for later use
    displayWindowThree.innerHTML = '<img src="' + prodArray[randNumThree].filePath +'" alt = "">';
  } else {
    globalRandNumThree = randNumThree;
    displayWindowThree.innerHTML = '<img src="' + prodArray[randNumThree].filePath +'" alt = "">';
  };
+ //3 lines below count the number of impressions per product
+ prodArray[globalRandNumOne].productImpressionsTracker++;
+ prodArray[globalRandNumTwo].productImpressionsTracker++;
+ prodArray[globalRandNumThree].productImpressionsTracker++;
 }
 
 randomProductsDisplay(); //calls the initial random product display
@@ -65,29 +77,39 @@ displayWindowTwo.addEventListener('click', handleWindowTwoClick);
 displayWindowThree.addEventListener('click', handleWindowThreeClick);
 
 function handleWindowOneClick (event) {
+  handleProductClick(); //adds to global click counter
   prodArray[globalRandNumOne].productClicksTracker++;
+  prodArray[globalRandNumOne].leftClickTracker++; //tracks where the image was displayed when it was clicked
+  globalLeftClickTracker++; //tracks which container was clicked
   console.log(prodArray[globalRandNumOne].productName+ ' was clicked. Its been clicked ' + prodArray[globalRandNumOne].productClicksTracker + ' times so far');
   randomProductsDisplay();
 }
 
 function handleWindowTwoClick (event) {
+  handleProductClick(); //adds to global click counter
   prodArray[globalRandNumTwo].productClicksTracker++;
+  prodArray[globalRandNumTwo].centerClickTracker++; //tracks where the image was displayed when it was clicked
+  globalCenterClickTracker++; //tracks which container was clicked
   console.log(prodArray[globalRandNumTwo].productName+ ' was clicked. Its been clicked ' + prodArray[globalRandNumTwo].productClicksTracker + ' times so far')
   randomProductsDisplay();
 }
 
 function handleWindowThreeClick (event) {
+  handleProductClick(); //adds to global click counter
   prodArray[globalRandNumThree].productClicksTracker++;
+  prodArray[globalRandNumThree].rightClickTracker++; //tracks where the image was displayed when it was clicked
+  globalRightClickTracker++; //tracks which container was clicked
   console.log(prodArray[globalRandNumThree].productName+ ' was clicked. Its been clicked ' + prodArray[globalRandNumThree].productClicksTracker + ' times so far')
   randomProductsDisplay();
 }
 
+//function below handles global click tracking
 function handleProductClick() {
-  if (globalClicksTracker < 16) {
+  if (globalClicksTracker < 14) {
   globalClicksTracker += 1;
   console.log('there have been ' + globalClicksTracker + ' global clicks so far');
 
 } else {
-  console.log('more than 14 clicks, trigger button yo');
+  document.getElementById('loadButton').className = "loadButton"; //loads button after specified number of global clicks
 };
 }
